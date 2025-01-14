66cc65e429b6a5381d8c8d70b7846495c5be0116
MitjaCH <mitja.kurath@mitja.io>
2025-01-14 10:19:48 +0100

    Add footer component & started working on url-shortener page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
+import { UsComponent } from './pages/utilities/us/us.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'us', component: UsComponent }
 ];
